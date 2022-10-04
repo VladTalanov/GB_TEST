cce5c1dfefbbcf9df74fcc9c2e2f7d08d27ddc4a
Влад Таланов <111172960+VladTalanov@users.noreply.github.com>
2022-10-04 09:08:53 +0300

    Добавление контрольной работы

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Контрольная работа 
+void FindWords (string[] words, int length)
+{
+    
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length <= length) Console.Write ($"{words[i]} ");
+    }
+}
+string[] array = {"priv", "kak", "de"};
+FindWords(array, 3);
